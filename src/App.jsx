f4505890a4efc5715147b91d6c5e3d9147bbdd81
jays_2712 <jaysoni.js2712@gmail.com>
2025-05-02 19:14:21 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter , Routes, Route } from "react-router-dom";
 import ItemDetail from "./components/ItemDetail";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
