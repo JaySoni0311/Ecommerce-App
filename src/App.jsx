9869415007a7f0a64b82bfddfa9f27d7f7252495
jays_2712 <jaysoni.js2712@gmail.com>
2025-05-02 11:03:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Checkout from "./components/Checkout";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="Ecommerce-App">
+      <BrowserRouter basename="eSTORE">
         <div>
           <Navbar />
         </div>
